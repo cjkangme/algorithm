f470aa7b73f50958b311ad955f84c1980a782c4b
cjkangme <nureongi0214@gmail.com>
2022-12-07 20:29:00 +0900

    Update memo.py

--- practice/memo.py
+++ practice/memo.py
@@ -1,22 +1,31 @@
-import sys
-input = sys.stdin.readline
-
-T = int(input())
-cnt = 0
-for t in range(T):
-    string = input()
-    chr_list = []
-    i = 0
-    is_group = True
-    for c in string:
-        if c not in chr_list:
-            chr_list.append(c)
+def devide(num):
+    if num % 2 == 0: #짝수일 경우
+        max_num = num // 2
+        for n in range(2, max_num + 1):
+            if num % n == 0:
+                return num//n, n
         else:
-            if string[i-1] == c:
-                pass
-            else:
-                is_group = False
-        i += 1
-    if is_group:
-        cnt += 1
-print(cnt)
+            return num, 0
+    else: #홀수일 경우
+        max_num = num // 3
+        for n in range(2, max_num + 1):
+            if num % n == 0:
+                return num//n, n
+        else:
+            return num, 0
+
+N = int(input())
+if N == 1:
+    pass
+else:
+    arr = []
+    while N != 1:
+        N, argu = devide(N)
+        if argu == 0:
+            arr.append(N)
+            break
+        arr.append(argu)
+    arr.sort()
+    for i in range(len(arr)):
+        print(arr[i])
+    
