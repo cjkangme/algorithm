637a548f1be4fce86a5f1b38da3c18c08122de96
cjkangme <nureongi0214@gmail.com>
2022-12-24 09:49:09 +0900

    Create 11576_Base Conversion.py

---
+++ boj/Silver_5/11576_Base Conversion.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/11576
+
+import sys
+input = sys.stdin.readline
+
+def a_to_ten(lst, b):
+    res = 0
+    for i, num in enumerate(lst):
+        base = b ** (m - i - 1)
+        res += num * base
+    return res
+
+def ten_to_b(num, b):
+    tmp = num
+    res = []
+    while tmp:
+        res.append(tmp % b)
+        tmp = tmp // b
+    return res
+
+A, B = map(int, input().split())
+m = int(input())
+num_list = list(map(int, input().split()))
+num_10 = a_to_ten(num_list, A)
+num_b = ten_to_b(num_10, B)
+print(*num_b[::-1])
