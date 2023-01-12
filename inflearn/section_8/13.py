b960f9aa30cc44a63e9ca746ab2f35c3d6e5f8a8
cjkangme <nureongi0214@gmail.com>
2023-01-12 11:25:51 +0900

    Update 13.py

--- inflearn/section_8/13.py
+++ inflearn/section_8/13.py
@@ -6,22 +6,27 @@ input = sys.stdin.readline
 
 MAX_NUM = 214700000
 
-if __name__=="__main__":
+if __name__ == "__main__":
     N = int(input())
     dis = [[MAX_NUM]*N for _ in range(N)]
+    for i in range(N):
+        dis[i][i] = 0
     score_list = [0] * N
     while True:
-        x, y = map(int,input().split())
-        if x==-1 and y==-1:
+        x, y = map(int, input().split())
+        if x == -1 and y == -1:
             break
         dis[x-1][y-1] = 1
         dis[y-1][x-1] = 1
+    # 플로이드-와샬 알고리즘
     for k in range(N):
         for i in range(N):
             for j in range(N):
                 dis[i][j] = min(dis[i][j], dis[i][k]+dis[k][j])
+    # 회장 후보 점수 구하고 출력
     for i in range(N):
-        score_list[i] = max(dis[i])
+        for j in range(N):
+            score_list[i] = max(score_list[i], dis[i][j])
     min_score = min(score_list)
     print(f'{min_score} {score_list.count(min_score)}')
     for i in range(N):
